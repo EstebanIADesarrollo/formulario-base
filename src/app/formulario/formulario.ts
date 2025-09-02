67c89bd84ea19a6fa1f3ce148084bd5a5d141fef
EstebanIADesarrollo <esteban.avh.test@gmail.com>
2025-09-02 13:20:18 -0500

    feat: add formulario component and configure routing

---
+++ src/app/formulario/formulario.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formulario',
+  standalone: true,
+  imports: [],
+  templateUrl: './formulario.html',
+  styleUrl: './formulario.scss'
+})
+export class Formulario {
+
+}
